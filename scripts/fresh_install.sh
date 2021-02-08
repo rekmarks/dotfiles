a478cbe70f1188bda3a7d18f6ce6529636aa5085
Erik Marks <rekmarks@protonmail.com>
2021-02-08 10:10:14 -0800

    Remove git hooks, eslintrc, update fresh_install

--- scripts/fresh_install.sh
+++ scripts/fresh_install.sh
@@ -1,22 +1,15 @@
-#! /bin/sh
+#!/usr/bin/env bash
 
 # create nvim dir in ~/.config if it does not exist
-if [ ! -d "~/.config/nvim"]
-then
-  mkdir -p "~/.config/nvim" # recursive mkdir, "p" for "parent"
-fi
+mkdir -p ~/.config/nvim
 
 # create nvm directory if it doesn't exist
-if [ ! -d "~/.nvm"]
-then
-  mkdir "~/.nvm"
-fi
+mkdir -p ~/.nvm
 
 # symlink .vimrc from neovim directory
 ln -s ~/.config/nvim/init.vim ~/.vimrc
 
 # install homebrew packages
-brew install yarn
 brew install nvim
 brew install tmux
 brew install git
@@ -32,7 +25,7 @@ xcode-select --install
 ./scripts/install/install_vim_plug.sh
 
 # nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 
 # tmux plugin manager (TPM)
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
