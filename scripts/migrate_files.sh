a457b144de53b8d29253a6f82c693017deee652b
Erik Marks <rekmarks@protonmail.com>
2023-04-09 11:58:11 -0700

    Add default cargo config

--- scripts/migrate_files.sh
+++ scripts/migrate_files.sh
@@ -15,6 +15,7 @@ migrate_files() {
   _eval "cp ./dotfiles/gpg/* ~/.gnupg" # nvm default packages
   _eval "cp ./dotfiles/misc/.vimrc ~/.config/nvim/init.vim" # neovim
   _eval "cp ./dotfiles/nvm/default-packages \"$NVM_DIR\"" # nvm default packages
+  _eval "cp ./dotfiles/cargo/config.toml ~/.cargo" # cargo config
   _eval "cp ./themes/oh-my-zsh/101.zsh-theme ~/.oh-my-zsh/custom/themes" # oh-my-zsh theme
 
   unset -f _eval
